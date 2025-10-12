0fb11c1b25d7ba859034e136c2fff8e65824759e
rostislavdurov380-ai <rostislavdurov380@gmail.com>
2025-10-12 16:20:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function startGame() {
     cactusInterval = setInterval(createCactus, 1500);
 
     if (birdInterval) clearInterval(birdInterval);
-  birdInterval = setInterval(createBird, 10000);
+  birdInterval = setInterval(createBird, 80000);
   
    if (litakInterval) clearInterval(litakInterval);
     litakInterval = setInterval(createlitak, 50000);
@@ -171,3 +171,4 @@ function endGame() {
 
 // Запуск гри
 startGame();
+
